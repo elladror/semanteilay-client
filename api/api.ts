7aa1aefaed4c442321b178440e0d6fb0edea6bbb
TopSecretSalt <78707249+TopSecretSalt@users.noreply.github.com>
2022-09-12 00:06:06 +0300

    13 route user to room after login if he had room id in query (#14)
    * deleted gitlab readme
    
    * removed unused imports
    
    * Create README.md
    
    * feat(userProvider, api): return to lobby if room in url doesnt exist
    
    * update gitignore
    
    * fix(api): fix error handling
    
    * feat(userProvider): if url leads to non existing room reroutes to lobby
    
    Co-authored-by: Amit <amit@Alfred.localdomain>

--- api/api.ts
+++ api/api.ts
@@ -1,46 +1,64 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { ApiError } from "next/dist/server/api-utils";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:9000";
 
 export const getApi =
   (baseUrl: string) =>
   async ({ path = "", payload = {} }) => {
-    const res = await axios.get(`${SERVER_URL + baseUrl + path}`, {
-      params: payload,
-    });
+    try {
+      const res = await axios.get(`${SERVER_URL + baseUrl + path}`, {
+        params: payload,
+      });
+      return res.data;
+    } catch (e) {
+      const res = (e as AxiosError).response;
+      if (res) throw new ApiError(res.status, res.data as string);
 
-    if (res.status !== 200) throw new Error(`${res.status}: ${res.statusText}`);
-
-    return res.data;
+      throw new ApiError(500, "unknown");
+    }
   };
 
 export const postApi =
   (baseUrl: string) =>
   async (payload = {}, path = "") => {
-    const res = await axios.post(`${SERVER_URL + baseUrl + path}`, payload);
+    try {
+      const res = await axios.post(`${SERVER_URL + baseUrl + path}`, payload);
+      return res.data;
+    } catch (e) {
+      const res = (e as AxiosError).response;
+      if (res) throw new ApiError(res.status, res.data as string);
 
-    if (res.status !== 200) throw new Error(`${res.status}: ${res.statusText}`);
-
-    return res.data;
+      throw new ApiError(500, "unknown");
+    }
   };
 
 export const patchApi =
   (baseUrl: string) =>
   async ({ payload = {}, path = "" }) => {
-    const res = await axios.patch(`${SERVER_URL + baseUrl + path}`, payload);
+    try {
+      const res = await axios.patch(`${SERVER_URL + baseUrl + path}`, payload);
+      return res.data;
+    } catch (e) {
+      const res = (e as AxiosError).response;
+      if (res) throw new ApiError(res.status, res.data as string);
 
-    if (res.status !== 200) throw new Error(`${res.status}: ${res.statusText}`);
-
-    return res.data;
+      throw new ApiError(500, "unknown");
+    }
   };
 
 export const deleteApi =
   (baseUrl: string) =>
   async (payload = {}, path = "") => {
-    const res = await axios.delete(`${SERVER_URL + baseUrl + path}`, payload);
+    try {
+      const res = await axios.delete(`${SERVER_URL + baseUrl + path}`, payload);
+      return res.data;
+    } catch (e) {
+      const res = (e as AxiosError).response;
+      if (res) throw new ApiError(res.status, res.data as string);
 
-    if (res.status !== 200) throw new Error(`${res.status}: ${res.statusText}`);
-
-    return res.data;
+      throw new ApiError(500, "unknown");
+    }
   };
+
 export const fetcher = (url: string, suffix = "") =>
   axios.get(url + suffix).then((res) => res.data);
